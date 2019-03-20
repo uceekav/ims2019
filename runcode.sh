cd0f091da3c597d8bc7df853582388ff3aacfd6c
uceekav <james.kavanagh.17@ucl.ac.uk>
2019-03-20 21:19:53 +0000

    ffmpeg and mp4box support from command line

--- runcode.sh
+++ runcode.sh
@@ -3,37 +3,43 @@
 ## Config File
 source `pwd`/config/config.cfg
 
-
-
 usage(){
 
 cat << EOF
-#########################################################################################
-#											#
-# usage: runcode.sh [-fhisv]								#	
-# -f FFMPEG flags e.g. -f "-c:v libvpx-vp9 -t 5"					#
-# -h This help message.									#
-# -i Ignores Pre checks									#
-# -s Segment times in ms, e.g. -s 1000,2000,3000  					#
-# -v Raw video file to encode								#
-#											#
-# Example:										#
-# This Command says encode the first 5 seconds of akiyo_qcif.y4m with libvpx-vp9.	#
-# Create two tests:									# 
-#	1. Segments are 1000 ms								#	
-#	2. Segments are 2000 ms 							#
-#											#
-# sudo ./runcode.sh -f "-c:v libvpx-vp9 -t 5" -i -s 1000,2000 -v static/akiyo_qcif.y4m	# 
-#											#
-#########################################################################################
+#################################################################################################
+#												#
+# usage: runcode.sh [-fhimv]									#		
+# -f FFMPEG flags e.g. -f "-c:v libvpx-vp9 -t 5"						#
+# -h This help message.										#
+# -i Ignores Pre checks										#
+# -m MP4Box flags:										#
+#	Note "--segment_name segment_" is not necessary.					#
+#    	"-dash" can support multiple values, e.g. "-dash 1000,2000,5000"  			#
+# -v Raw video file to encode									#
+#												#
+# Example:											#
+# This Command says encode the first 5 seconds of akiyo_qcif.y4m with libvpx-vp9.		#
+# Create two tests:										# 
+#	1. Segments are 1000 ms									#		
+#	2. Segments are 2000 ms 								#
+#												#
+# sudo ./runcode.sh -f "-c:v libvpx-vp9 -t 5" -i -m "-dash 1000,2000" -v static/akiyo_qcif.y4m	# 
+#												#
+#################################################################################################
 EOF
 
 }
 
+print_line (){
+echo "###################################################################################"
+}
+
 pre_checks (){
 
+	print_line
+	echo -e "###################     STEP 1 - PRECHECKS                      ###################"
+	print_line
 
-	echo -e "\n###################     STEP 1 - PRECHECKS                      ###################\n"
 	## check ffmpeg
 	which ffmpeg
 	
@@ -41,9 +47,9 @@ pre_checks (){
 	
 		## Use which ffmpeg install
 		FFMPEG=`which ffmpeg`
-		echo "FFMPEG PATH is: ${FFMPEG}"
+		echo -e "\nFFMPEG PATH is: ${FFMPEG}"
 	else
-		echo "FFMPEG PATH is: ${FFMPEG}"
+		echo -e "\nFFMPEG PATH is: ${FFMPEG}"
 	fi
 
 	## Shaka Dir
@@ -92,42 +98,21 @@ pre_checks (){
 		exit 1
 	fi
 
-	echo -e "\e[32mPRECHECKS COMPLETE\e[0m\n"
+	echo -e "\n\e[32mPRECHECKS COMPLETE\e[0m\n"
 
-}
-
-parse_encoding (){
-
-	##TODO BUT IN A BETTER CHECK
-	echo -e "\n###################     STEP 2 - PARSE ENCODING                 ##################\n"
-
-	if [[ $FFMPEGFLAGS =~ .*libx264.* ]];
-	then
-		ENCODING=libx264
-	elif [[ $FFMPEGFLAGS =~ .*libx265.* ]];
-	then
-		ENCODING=libx265
-	elif [[ $FFMPEGFLAGS =~ .*libvpx-vp9.* ]]
-	then
-		ENCODING=libvpx-vp9
-	else
-		echo "Unable to get encoding from -f flag."
-		usage
-		exit 1
-	fi
-	echo "Encoding Set to $ENCODING"
-	echo -e "\e[32mPARSE ENCODING COMPLETE\e[0m\n"
 }
 
 generate_mp4 (){
 
-	echo -e "\n###################     STEP 3 - GENERATING MP4                 ##################\n"
+	print_line
+	echo -e "###################     STEP 2 - GENERATING MP4                 ###################"
+	print_line
 
 	encoding=$ENCODING
 
 	if [[ -z $YUVVID ]];
 	then
-		echo "-v is Mandatory! Exiting...."
+		echo -e "\n-v is Mandatory! Exiting...."
 		usage
 		exit 1
 	fi
@@ -135,7 +120,7 @@ generate_mp4 (){
 	video="`pwd`/$YUVVID"
 
 	if [[ ! -f $video ]];then
-		echo "$video does not exist"
+		echo -e "\n${video} does not exist"
 		usage
 		exit 1
 	fi
@@ -145,13 +130,14 @@ generate_mp4 (){
 
 	vp9="libvpx-vp9"
 	x264="libx264"
+	x265="libx265"
 
-
-	if [[ ( "$encoding" == "$vp9" || "$encoding" == "$x264" ) ]];
+	if [[ ( "$encoding" == "$vp9" || "$encoding" == "$x264"  || "$encoding" == "$x265" ) ]];
 	then
 		mkdir -p $encoding
 		
 		cat <<-EOF
+
 			Creating mp4 video from $video with $encoding.
 			FFMPEG LOG located here: ${VIDDIR}/ffmpeg_${encoding}.output
 		EOF
@@ -170,21 +156,18 @@ generate_mp4 (){
 		exit 1
 	fi	
 
-	echo -e "\e[32mGENERATING MP4 COMPLETE\e[0m\n"
+	echo -e "\n\e[32mGENERATING MP4 COMPLETE\e[0m\n"
 }
 
 
 generate_dash (){
 
 
-	echo -e "###################     STEP 4 - SEGMENT AND CREATE MPD         ##################\n"
+	print_line
+	echo -e "###################     STEP 3 - SEGMENT AND CREATE MPD         ###################"
+	print_line
 
 	encoding=$ENCODING
-	if [ -z $SEGMENTSIZE ];then
-        	echo -e "No segment times provided! Please see Usage below. \e[31mExiting....\e[0m"
-		usage
-		exit 1
-	fi
 
 	segmentlist=(`echo $SEGMENTSIZE | tr ',' ' '`)
 
@@ -201,14 +184,21 @@ generate_dash (){
 		mkdir -p $tmpdir
 		cd $tmpdir
 
-		MP4Box -dash ${segmenttime} -segment-name segment_ $VIDDIR/$encoding/video_${encoding}.mp4 &> ./MP4Box_${segmenttime}.output
-       		
+		## Check if there's extra commands for MP4Box command
+		if [[ -z $MP4REMAININGCMD ]];
+		then
+			MP4Box -dash ${segmenttime} -segment-name segment_ $VIDDIR/$encoding/video_${encoding}.mp4 &> ./MP4Box_${segmenttime}.output
+		else
+			MP4Box -dash ${segmenttime} -segment-name segment_ $MP4REMAININGCMD $VIDDIR/$encoding/video_${encoding}.mp4 &> ./MP4Box_${segmenttime}.output
+		fi
+
 		cp $CONFIGDIR/myapp.js $CONFIGDIR/runtest.html $VIDDIR/$encoding/seg_${segmenttime}/
 		cd $VIDDIR/$encoding/seg_${segmenttime}/
 		# Edit files
 		sed -i "s|MPDGOESHERE|$mpd|g" myapp.js
 
 		cat <<-EOF
+
 			Creating $segmenttime ms segments and dash files
 			MPD and Segments are here: $tmpdir
 			See output from MP4box here $tmpdir/MP4Box_${segmenttime}.output
@@ -222,7 +212,10 @@ generate_dash (){
 
 generate_shaka (){
 
-	echo -e "################### STEP 5 - RUN TESTS ONE AT A TIME ###################\n"
+	print_line
+	echo -e "################### STEP 4 - RUN TESTS ONE AT A TIME		###################"
+	print_line
+
 
 	## Need to move files in place
 	segmentlist=(`echo $SEGMENTSIZE | tr ',' ' '`)
@@ -237,7 +230,7 @@ generate_shaka (){
 	for ((i=0;i<$NUMBEROFTESTSEG;i++));
         do
 		segment=${segmentlist[i]}
-		echo "Moving files for ${segment} ms segment test into $PUBLICHTML"
+		echo -e "\nMoving files for ${segment} ms segment test into $PUBLICHTML"
 		
 		## copy the files into place
 		rm -f $PUBLICHTML/segment_* > /dev/null
@@ -264,6 +257,7 @@ generate_shaka (){
 					echo -e "\nexiting..."
 					exit 0
 				fi
+				echo ""
 			fi
 		fi	
         done
@@ -271,9 +265,10 @@ generate_shaka (){
 	echo -e "\e[32mRUN TESTS ONE AT A TIME COMPLETE\e[0m\n"
 }
 
+SKIP=false
 IGNORE=false
 
-while getopts "f:his:v:" key;
+while getopts "f:him:v:" key;
 do
 	case $key in
 		f)
@@ -286,13 +281,8 @@ do
 		i)
 			IGNORE=true
 			;;
-		s)
-			SEGMENTSIZE=$OPTARG
-			if [ -z $SEGMENTSIZE ];then
-                		echo "-s is Mandatory! Exiting...."
-				usage
-                		exit 1
-			fi
+		m)
+			MP4BOXFLAGS=$OPTARG
 			;;
 		v)
 			YUVVID=$OPTARG
@@ -310,7 +300,9 @@ if ! $IGNORE; then
 	pre_checks
 else 
 	cat <<-EOF
-		#############  STEP 1 - SKIPPING PRECHECKS ###################
+		##################################################################################
+		###################	STEP 1 - SKIPPING PRECHECKS		################## 
+		##################################################################################
 
 		-i flag supplied in the command line. Skipping pre checks
 
@@ -318,8 +310,20 @@ else
 
 fi
 
-parse_encoding
+ENCODING="`echo $FFMPEGFLAGS | perl -nle 'm/-c\:v ([A-za-z\-0-9]*)/; print $1' | grep -i [a-z]`"
+SEGMENTSIZE="`echo $MP4BOXFLAGS | perl -nle 'm/-dash ([0-9\,]*)/; print $1'`"
+
+if [ -z $SEGMENTSIZE ];then
+        
+	## No point going on if no segment times passed in.
+	echo -e "\nNo segment times provided! Please see Usage below. \e[31mExiting....\e[0m\n"
+        usage
+        exit 1
+fi
+
+DASHSIZE="`echo $MP4BOXFLAGS | perl -nle 'm/(-dash [0-9\,]*)/; print $1'`"
+MP4REMAININGCMD="`echo $MP4BOXFLAGS | sed "s/${DASHSIZE}//"`" 
+
 generate_mp4 
 generate_dash 
 generate_shaka
-
